048ef9c02c31403efc487a82513c7d75610a0ed1
Gde-G <gdguernica@gmail.com>
2024-02-07 21:42:51 -0300

    S3 media and static url

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -179,7 +179,7 @@ AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 AWS_S3_FILE_OVERWRITE = bool(int(os.environ.get('AWS_S3_FILE_OVERWRITE')))
 
 STATIC_URL = f'{AWS_S3_CUSTOM_DOMAIN}/static/'
-MEDIA_URL = f'{AWS_S3_CUSTOM_DOMAIN}/media/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 MEDIA_ROOT = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 STORAGES = {
